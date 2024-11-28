7b8b3cd736a92157a0cd2aeca25dad7384d579b5
Groumpfyy <no@email>
2024-11-28 15:18:32 -0600

    Fix for version 24

--- VPv3r/keymap.c
+++ VPv3r/keymap.c
@@ -411,6 +411,7 @@ tap_dance_action_t tap_dance_actions[] = {
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 
 // This globally defines all key overrides to be used
-const key_override_t *key_overrides[] = {
-	&delete_key_override
+const key_override_t **key_overrides = (const key_override_t *[]){
+	&delete_key_override,
+    NULL
 };
